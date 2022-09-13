08e38be75801f823242d983ed9e96cc4eca73045
vivekkushal <vivekjss1@gmail.com>
2022-09-13 22:45:44 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# 📈 Stock Profit or Loss 📈
+
+# profit or loss on your stock portfolio?
+
+# How to use:
+
+- Enter stocks price you bought
+
+- Enter the quantity of stocks you bought
+
+- Enter the current price of your stocks
+
+- Click on the **Calculate** button
+
+- Output will be shown below the button after its calculated.
+
+# Technologies Used
+
+- HTML
+
+- CSS
+
+- JavaScript
---
+++ app.js
@@ -0,0 +1,31 @@
+const initialPrice = document.querySelector("#initial-price");
+const stocksQuantity = document.querySelector("#stocks-quantity");
+const currentPrice = document.querySelector("#current-price");
+const btnCheck = document.querySelector(".btn-check");
+const output = document.querySelector(".output-stocks");
+
+function profitOrLoss() {
+  let initialPriceNum = +initialPrice.value;
+  let stocksQuantityNum = +stocksQuantity.value;
+  let currentPriceNum = +currentPrice.value;
+
+  let cp = initialPriceNum * stocksQuantityNum;
+  let sp = currentPriceNum * stocksQuantityNum;
+
+  if (sp > cp) {
+    let profit = sp - cp;
+    let profitPer = (profit / cp) * 100;
+    output.style.color = "green";
+    output.innerText = `Your profit is Rs. ${profit} and profit percentage is ${profitPer}%`;
+  } else if (sp < cp) {
+    let loss = cp - sp;
+    let lossPer = (loss / cp) * 100;
+    output.style.color = "red";
+    output.innerText = `Your loss is Rs. ${loss} and loss percentage is ${lossPer}%`;
+  } else {
+    output.style.color = "blue";
+    output.innerText = "No pain, No gain and No gain, No pain.";
+  }
+}
+
+btnCheck.addEventListener("click", profitOrLoss);
