d8f508f0cb033bb81fbead9987a54f756ada459f
vivekkushal <vivekjss1@gmail.com>
2022-09-14 20:29:48 +0530

    fix: negative input avoided

--- app.js
+++ app.js
@@ -12,19 +12,25 @@ function profitOrLoss() {
   let cp = initialPriceNum * stocksQuantityNum;
   let sp = currentPriceNum * stocksQuantityNum;
 
-  if (sp > cp) {
-    let profit = sp - cp;
-    let profitPer = (profit / cp) * 100;
-    output.style.color = "green";
-    output.innerText = `Your profit is Rs. ${profit} and profit percentage is ${profitPer}%`;
-  } else if (sp < cp) {
-    let loss = cp - sp;
-    let lossPer = (loss / cp) * 100;
-    output.style.color = "red";
-    output.innerText = `Your loss is Rs. ${loss} and loss percentage is ${lossPer}%`;
+  if (initialPriceNum > 0 && currentPriceNum > 0 && stocksQuantityNum > 0) {
+    if (sp > cp) {
+      let profit = sp - cp;
+      let profitPer = (profit / cp) * 100;
+      output.style.color = "green";
+      output.innerText = `Your profit is Rs. ${profit} and profit percentage is ${profitPer}%`;
+    } else if (sp < cp) {
+      let loss = cp - sp;
+      let lossPer = (loss / cp) * 100;
+      output.style.color = "red";
+      output.innerText = `Your loss is Rs. ${loss} and loss percentage is ${lossPer}%`;
+    } else {
+      output.style.color = "blue";
+      output.innerText = "No pain, No gain and No gain, No pain.";
+    }
   } else {
-    output.style.color = "blue";
-    output.innerText = "No pain, No gain and No gain, No pain.";
+    output.style.color = "#94a3b8";
+    output.innerText =
+      "Initial Price, Current Price or Stocks Quantity can not be negative";
   }
 }
 
